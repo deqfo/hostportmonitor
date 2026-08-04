3576e9a08b6ca0ea2e304aa4abe442006d3c70ce
Kristián Eliaš <kristiandeqfo@gmail.com>
2026-08-04 10:10:07 +0200

    Created basic config with ip

--- src/main.py
+++ src/main.py
@@ -1,2 +1,14 @@
-def gen():
-    pass
+import json
+import os
+
+
+def load_config(filepath: str) -> list[dict]:
+    if not os.path.exists(filepath):
+        raise FileNotFoundError(f"Súbor {filepath} nebol nájdený.")
+
+    with open(filepath, "r", encoding="utf-8") as f:
+        return json.load(f)
+
+
+config_path = "data/config.json"
+print(load_config(config_path))
